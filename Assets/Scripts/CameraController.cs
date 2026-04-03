e541d9b3d60312ea637235ec37c0552ef861a0c1
IslamSoylu <islamsylu@outlook.com>
2026-04-03 04:58:53 +0300

    coinleri ekledim toplanabilir yapıcam şimdi

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class CameraController : MonoBehaviour
 {
-    
+    public GameObject player; //will reference the player game object's position
+    private Vector3 offset; //will set the offset position from the player to the camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calculate the offset position between the camera and the player at the start of the game
+        //it subtracts the player's position from the camera's
+        offset = transform.position - player.transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets the camera to where the player is plus the offset set above
+        transform.position = player.transform.position + offset;
+    }
 }
